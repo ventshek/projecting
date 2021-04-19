e09504177921380653b8c3ed74190cb1c7e2246d
ventshek <82648360+ventshek@users.noreply.github.com>
2021-04-19 18:38:27 +0200

    Update doinstall.sh

--- doinstall.sh
+++ doinstall.sh
@@ -21,6 +21,7 @@ echo '==== Install packages ===='
 wget https://launcher.mojang.com/v1/objects/1b557e7b033b583cd9f66746b7a9ab1ec1673ced/server.jar
 mv server.jar minecraft_server_1.16.5.jar
 java -Xms1024M -Xmx2048M -jar minecraft_server_1.16.5.jar nogui
+xdotool key ctrl+c
 rm eula.txt
 cat > eula.txt
 eula=true
